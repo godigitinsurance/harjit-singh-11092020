ffb6307481a08652451ce19b2ef915c29eaa5b5c
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-09-11 08:07:27 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ $("#endDate").text("10-Sep-2021");
 $("#type").text("Four Wheeler");
 $("#registrationNumber").text("PB06P0071");
 $("#make").text("TATA MOTORS");
-$("#model").text("SAFARI / ");
+$("#model").text("SAFARI");
 $("#vehicalSubType").text("MINOR FACELIFT LX 4X");
 $("#engineNo").text("22LDICOR09PYYJ26271");
 $("#chassis").text("MAT403715BNP16296");
