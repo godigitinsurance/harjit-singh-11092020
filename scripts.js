c7027f4915d3146e2c12eb9504fa5b1c5cda6967
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-09-11 07:58:20 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Harjit Singh ");
+$("#policyNumber").text("BZ7449090");
+$("#startDate").text("11-Sep-2020");
+$("#endDate").text("10-Sep-2021");
+
+
+$("#type").text("Four Wheeler");
+$("#registrationNumber").text("PB06P0071");
+$("#make").text("TATA MOTORS");
+$("#model").text("SAFARI / ");
+$("#vehicalSubType").text("MINOR FACELIFT LX 4X");
+$("#engineNo").text("22LDICOR09PYYJ26271");
+$("#chassis").text("MAT403715BNP16296");
